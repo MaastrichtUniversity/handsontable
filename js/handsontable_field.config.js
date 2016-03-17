6e9b0e8278655a2a2706e80461fa01ea88be31fb
Paul van Schayck <polleke@gmail.com>
2016-03-17 15:25:42 +0100

    Allow to enter multiple rows of handsontable into a xml form

--- js/handsontable_field.config.js
+++ js/handsontable_field.config.js
@@ -1,62 +1,59 @@
 Drupal.behaviors.handsontable = {
     attach: function (context, settings) {
 
-        function updateTable(table, container) {
-            var datadump = jQuery(table).handsontable('getData'),
-                dataString = JSON.stringify(datadump);
-            jQuery(container).val(dataString);
+        // Adds all table rows and columns to input fields, so the Drupal form processor can handle it
+        function updateTable(name, table, container, columns) {
+            var data = jQuery(table).handsontable('getData');
+
+            jQuery(container).empty();
+
+            jQuery(data).each(function (row) {
+                jQuery(columns).each(function (column, columnName) {
+                    if (data[row][column] != null && data[row][column] != "") {
+                        jQuery(container).append(
+                            jQuery("<input type='hidden' name='" + name + "[" + row + "][" + columnName + "]'>").val(data[row][column])
+                        )
+                    }
+                });
+            });
+        }
+
+        // Trigger AJAX callback
+        function triggerCallback(container) {
+            jQuery(container + '-callback')
+                .trigger("mousedown")
         }
 
         // Initiate handsontable and add constructor options.
-        jQuery.each(Drupal.settings.handsontable.ids, function(i, val) {
-            var container = '#' + val,
+        jQuery.each(settings.handsontable.names, function (i, name) {
+            var container = '#edit-' + name,
                 table = container + '-table';
 
-            jQuery(table).handsontable({
-                data: JSON.parse(Drupal.settings.handsontable.data[i]),
-                minSpareRows: 1,
-                startCols: Drupal.settings.handsontable.colHeaders[i].length,
-                startRows: Drupal.settings.handsontable.startRows[i],
-                colHeaders: Drupal.settings.handsontable.colHeaders[i],
-                columnSorting: true,
-                contextMenu: ['remove_row', 'undo'],
-                stretchH: 'all',
-                allowInsertColumn: false,
-                allowRemoveColumn: false,
-                outsideClickDeselects: false,
-                persistentState : true,
-                beforeChange: function () {
-                    updateTable(table, container);
-                },
-                afterChange: function () {
-                    updateTable(table, container);
-                },
-                afterRemoveRow: function() {
-                    updateTable(table, container);
-                },
-                afterRemoveCol: function() {
-                    updateTable(table, container);
-                }
-            });
-        });
+            jQuery(table, context).once('processed', function () {
+                jQuery(table).handsontable({
+                    data: JSON.parse(settings.handsontable.data[i]),
+                    minSpareRows: 1,
+                    startCols: settings.handsontable.colHeaders[i].length,
+                    startRows: settings.handsontable.startRows[i],
+                    colHeaders: settings.handsontable.colHeaders[i],
+                    columnSorting: true,
+                    contextMenu: ['remove_row', 'undo'],
+                    stretchH: 'all',
+                    allowInsertColumn: false,
+                    allowRemoveColumn: false,
+                    outsideClickDeselects: false,
+                    persistentState: true,
+                    afterChange: function (changes, source) {
+                        var columns = settings.handsontable.columns[i];
+                        updateTable(name, table, container, columns);
 
-        var actions = {
-            addRow: function (event, ht) {
-                ht.alter('insert_row');
-            }
-        };
-
-        jQuery(document.body).delegate('div.handsontable-container a[data-action]','click', function (event) {
-            var action = jQuery(this).data('action'),
-                name =  jQuery(this).parents('div.handsontable-container').find('div.handsontable').attr('id'),
-                ht = jQuery('#' + name).handsontable('getInstance');
-
-            event.preventDefault();
-
-            // If there's an action with the given name, call it
-            if( typeof actions[action] === 'function' ) {
-                actions[action].call(this, event, ht, name);
-            }
+                        // Ignore the change made by the loading of data
+                        if (source != "loadData") {
+                            triggerCallback(container)
+                        }
+                    }
+                });
+            })
         });
     }
 };
